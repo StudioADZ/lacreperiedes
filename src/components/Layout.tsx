e8a1a108f5205003b42630aa90de06143a0fcc6f
StudioADZ <jjgo838@gmail.com>
2026-01-18 23:22:38 +0100

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,6 +9,9 @@ const Layout = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const location = useLocation();
 
+  const isAdminRoute = location.pathname.startsWith("/admin");
+  const showAssistant = !isAdminRoute;
+
   // Close menu on route change
   useEffect(() => {
     setMenuOpen(false);
@@ -19,18 +22,20 @@ const Layout = () => {
     window.scrollTo(0, 0);
   }, [location.pathname]);
 
-  // Don't show assistant on admin page
-  const showAssistant = !location.pathname.startsWith('/admin');
-
   return (
     <div className="min-h-screen bg-background">
       <Header onMenuClick={() => setMenuOpen(true)} />
       <DrawerMenu open={menuOpen} onOpenChange={setMenuOpen} />
-      
-      <main>
+
+      {/* 
+        SAFE: compense Header fixed + StickyBar fixed
+        - pt-16 : ajuste si ton header n'est pas ~64px
+        - pb-24 : ajuste selon la hauteur de StickyBar
+      */}
+      <main className="pt-16 pb-24">
         <Outlet />
       </main>
-      
+
       <StickyBar />
       {showAssistant && <AssistantChat />}
     </div>
